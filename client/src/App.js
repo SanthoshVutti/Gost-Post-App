3a9b4f3f24307f98865387b9f37b999d2580e210
SanthoshVutti <147086166+SanthoshVutti@users.noreply.github.com>
2026-02-03 18:42:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { formatDistanceToNow } from "date-fns";
 import SecretForm from "./SecretForm"; // <-- add this import
-
+const API_BASE=process.env.REACT_APP_API_URL ||'http://localhost:5000';
 function App() {
   const [secrets, setSecrets] = useState([]);
   const [likingId, setLikingId] = useState(null);
