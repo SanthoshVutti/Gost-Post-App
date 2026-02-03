8cafbae738393c23feb035bd4ed61f123bd6ea16
SanthoshVutti <147086166+SanthoshVutti@users.noreply.github.com>
2026-02-03 19:26:42 +0530

    Refactor getSecrets to streamline secret retrieval

--- SERVER/controllers/secretController.js
+++ SERVER/controllers/secretController.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const getSecrets = async (req,res)=>{
     try{
-        const secrets=(await Secret.find()).sort({createdAt:-1});
+        const secrets=await Secret.find().sort({createdAt:-1});
         res.json(secrets);
     } catch (error){
         console.log("Error at get ",error);
